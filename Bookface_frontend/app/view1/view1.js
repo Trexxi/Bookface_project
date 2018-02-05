aef997ca9f36529016bc49e3a0da19ec77451b93
Beatech <beatrice.vilhelmsson@fmtechnology.se>
2018-02-05 13:05:14 +0100

    Fixed from view2 to vieew 1

--- Bookface_frontend/app/view1/view1.js
+++ Bookface_frontend/app/view1/view1.js
@@ -20,6 +20,58 @@ angular.module('myApp.view1', ['ngRoute'])
     console.log(error);
   });
 
+    $scope.postData = function() {
+        var data = {
+            firstName: "OLLLLOF",
+            lastName: "Nah",
+            isTired: true
+        };
+
+        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+        function serializeData(data) {
+            // If this is not an object, defer to native stringification.
+            if (!angular.isObject(data)) {
+                return ((data == null) ? "" : data.toString());
+            }
+
+            var buffer = [];
+
+            // Serialize each key in the object.
+            for (var name in data) {
+                if (!data.hasOwnProperty(name)) {
+                    continue;
+                }
+
+                var value = data[name];
+
+                buffer.push(
+                    encodeURIComponent(name) + "=" + encodeURIComponent((value == null) ? "" : value)
+                );
+            }
+
+            // Serialize the buffer and clean it up for transportation.
+            var source = buffer.join("&").replace(/%20/g, "+");
+            return (source);
+        }
+
+
+        $http.post('http://localhost:3000/users/newCard', serializeData({
+            firstName: "snekenskvbokeof",
+            lastName: "ewfwepkfewkofokewkfoewpo",
+            isTired: false
+        }), {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+            .success(function (data, status, headers, config) {
+                console.log("bra jobbat");
+            })
+            .error(function (data, status, header, config) {
+                alert("XD");
+            });
+    }
+
+
+
+
     $scope.areaOfEffect = function(){
         var textElement = angular.element(document.querySelector('#messageArea'));
         //console.log(textElement);
@@ -45,6 +97,7 @@ angular.module('myApp.view1', ['ngRoute'])
         if(textInBox.length !== 0){
             //HERE POST THINGY!
             $scope.myText ='';
+            $scope.postData();
             $scope.notAreaOfEffect();
         } else {
             alert("Can't send empty message");
